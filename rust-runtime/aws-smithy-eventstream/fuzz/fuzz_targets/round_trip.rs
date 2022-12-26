d595019cc0f8feb688ce1a143b45e4172291eff5
ysthakur <2004yst@gmail.com>
2022-12-25 23:26:04 -0500

    Don't bother checking the ErrorKind

--- rust-runtime/aws-smithy-eventstream/fuzz/fuzz_targets/round_trip.rs
+++ rust-runtime/aws-smithy-eventstream/fuzz/fuzz_targets/round_trip.rs
@@ -8,27 +8,11 @@
 use aws_smithy_eventstream::frame::Message;
 use libfuzzer_sys::fuzz_target;
 
-const EXPECTED_ERRORS: [&str; 5] = [
-    "HeadersTooLong",
-    "PayloadTooLong",
-    "MessageTooLong",
-    "InvalidHeaderNameLength",
-    "TimestampValueTooLarge",
-];
-
 fuzz_target!(|message: Message| {
     let mut buffer = Vec::new();
-    match message.write_to(&mut buffer) {
-        Err(err) => {
-            let err_dbg = format!("{err:?}");
-            if !EXPECTED_ERRORS.iter().any(|e| e.contains(&err_dbg)) {
-                panic!("unexpected error on write: {}", err)
-            }
-        }
-        Ok(_) => {
-            let mut data = &buffer[..];
-            let parsed = Message::read_from(&mut data).unwrap();
-            assert_eq!(message, parsed);
-        }
+    if message.write_to(&mut buffer).is_ok() {
+        let mut data = &buffer[..];
+        let parsed = Message::read_from(&mut data).unwrap();
+        assert_eq!(message, parsed);
     }
 });
